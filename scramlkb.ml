e5dbb2e752c46966e8b5a1c9d1d7439b01e45cf8
Your Name <you@example.com>
2017-07-04 15:27:15 +0200

    Some cleaning up

--- scramlkb.ml
+++ scramlkb.ml
@@ -11,20 +11,20 @@ type scramlkb_operation =
 | Plaintext of ((string -> unit) * string)
 | Scrambled of ((string -> unit) * string)
 
-let rec _unbiased_modulo ~(f:unit -> char) (element_count:int) : int=
-  (** returns a char -1 < c < element_count **)
-  let r = int_of_char( f () ) in
-    if 256 mod element_count = 0 then
-      r mod element_count
-    else begin
-      if r < element_count then r (* the number is in our keyspace *)
-      else _unbiased_modulo f element_count (* try again *)
-    end
-
-let _random_unbiased_modulo () : (int -> int)=
+let random_char =
   (** opens an entropy channel and returns a reader **)
   let random_channel = open_in_bin "/dev/urandom" in
   let f () = (input_char random_channel) in
+  let rec _unbiased_modulo ~(f:unit -> char) (element_count:int) : int=
+    (** returns a char -1 < c < element_count **)
+    let r = int_of_char( f () ) in
+      if 256 mod element_count = 0 then
+        r mod element_count
+      else begin
+        if r < element_count then r (* the number is in our keyspace *)
+        else _unbiased_modulo f element_count (* try again *)
+      end
+  in
   _unbiased_modulo ~f
 
 let get1char () : (char)=
@@ -90,12 +90,12 @@ let display_layout ~display ~alphabet ~remapped_alphabet ~prompt =
       )
     )
 
-let swap arr i j =
-  let temp = arr.[i] in
-    Bytes.set arr i arr.[j];
-    Bytes.set arr j temp
- 
 let fisher_yates_shuffle ~random_char remapped =
+  let swap arr i j =
+    let temp = arr.[i] in
+      Bytes.set arr i arr.[j];
+      Bytes.set arr j temp
+  in
   let l = Bytes.length remapped in
     for i = (l-1) downto 1 do
       let r = random_char (i+1) in
@@ -103,7 +103,13 @@ let fisher_yates_shuffle ~random_char remapped =
     done
     ; remapped
 
-let retrieve_key_entry ~random_char ~display ~prompt =
+let display s = Printf.fprintf (Unix.out_channel_of_descr Unix.stderr) "%s%!" s
+
+let retrieve_key_entry ~prompt =
+  let linux_cls     = "\x1b[2J"     (* clear screen:   tput clear *)
+      and linux_save    = "\x1b[?1049h" (* save screen:    tput smcup *)
+      and linux_restore = "\x1b[?1049l\x1b[0m" in (* restore screen: rmcup *)
+  let () = display (linux_save ^ linux_cls) in
   let alphabet      = "~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:\"ZXCVBNM<>?`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./" in
   let rec get_char (output : string) =
     let remapped_alphabet =
@@ -124,9 +130,12 @@ let retrieve_key_entry ~random_char ~display ~prompt =
     | c ->
       get_char (output ^ (String.make 1 c))
   in
-    get_char ""
+  let output = get_char "" in
+  let () = display (linux_cls ^ linux_restore) in
+  output
 
-let systemd_askpassword ~display reply_socket passphrase =
+let systemd_askpassword ~prompt reply_socket =
+  let passphrase = retrieve_key_entry ~prompt in
   (* debug: let () = display ("got passphrase: '"^ passphrase ^"' -- "^ reply_socket) in *)
   let sock = Unix.(socket PF_UNIX SOCK_DGRAM 0) in
   (* TODO: this may fail due to permissions. solution? *)
@@ -135,15 +144,19 @@ let systemd_askpassword ~display reply_socket passphrase =
   let passphrase = "+" ^ passphrase in (* systemd: "+" means "entry was a success" *)
   let _ = Unix.send sock passphrase 0 (String.length passphrase) [] in
   Unix.(shutdown sock SHUTDOWN_ALL)
-  
-let systemd_loopforever ~askpassword_dir ~inotify_fd ~event_list ~cb =
+
+let systemd_watch ~event_list =
+  let open Inotify in
+  let inotify_fd = Inotify.create () in
+  let askpassword_dir = "/run/systemd/ask-password/" in
+  let _ = Inotify.add_watch inotify_fd askpassword_dir [ S_Moved_to ; S_Close_write ] in
   let rec inotify_read () = 
     begin
       try Inotify.read inotify_fd
       with _ -> inotify_read ()
     end
   in
-  let rec get_password_request = function
+  let rec get_password_request : 'a list -> unit = function
   | []     -> get_password_request (inotify_read ()) (* get new events *)
   | current_event :: event_tl ->
     begin match current_event with
@@ -151,7 +164,6 @@ let systemd_loopforever ~askpassword_dir ~inotify_fd ~event_list ~cb =
       (Str.first_chars file_name 4) = "ask."
       ->
        let blank_msg = {message = ""; socket = ""; error = true } in
-       let return =
        begin try
          let msg =
            let fh = open_in (askpassword_dir ^ file_name) in
@@ -162,7 +174,7 @@ let systemd_loopforever ~askpassword_dir ~inotify_fd ~event_list ~cb =
              let pair = Str.bounded_split (Str.regexp "=") line 2 in
              (List.hd pair, List.tl pair)
              in
-             let acc = begin match lowercase hd with
+             let acc = begin match lowercase_ascii hd with
              | "message" -> {acc with message = List.hd tl}
              | "socket"  -> {acc with socket  = List.hd tl}
              | _ -> acc
@@ -171,28 +183,31 @@ let systemd_loopforever ~askpassword_dir ~inotify_fd ~event_list ~cb =
            in
              readall blank_msg
          in
-           [Scrambled ((cb msg.socket) , msg.message)]
+           systemd_askpassword ~prompt:msg.message msg.socket
        with
-       | Sys_error _ -> [] end (* TODO currently failing silently *)
-     in
-       return @ [Systemdwatch event_tl]
+       | Sys_error _ -> () (* TODO currently failing silently *)
+       end
     | _ ->
       get_password_request event_tl (* event read error *)
     end
   in get_password_request []
 
+let handle_operation = function
+  | Plaintext (callback, prompt) ->
+      let () = display prompt in
+      callback ((input_line (in_channel_of_descr stdin)))
+  | Scrambled (callback, prompt) ->
+      let passphrase = retrieve_key_entry ~prompt in
+      callback passphrase
+  | Systemdwatch event_list ->
+      systemd_watch ~event_list
+
 let () =
-  let random_char = _random_unbiased_modulo () in
-    let output  s = Printf.fprintf (Unix.out_channel_of_descr Unix.stdout) "%s%!" s
-    and display s = Printf.fprintf (Unix.out_channel_of_descr Unix.stderr) "%s%!" s in
-    let output_nl s   = output (s ^ "\n")
-    and linux_cls     = "\x1b[2J"     (* clear screen:   tput clear *)
-    and linux_save    = "\x1b[?1049h" (* save screen:    tput smcup *)
-    and linux_restore = "\x1b[?1049l\x1b[0m" (* restore screen: rmcup *)
-    in
-    let rec parse_cmdline_queue acc = function
-      | hd::tl ->
-        let mode =
+  let output  s = Printf.fprintf (Unix.out_channel_of_descr Unix.stdout) "%s%!" s in
+  let output_nl s = output (s ^ "\n") in
+  let rec parse_cmdline_queue (acc : scramlkb_operation list) = function
+      | (hd::tl) ->
+        let mode : scramlkb_operation =
           ( match hd with
             | "p" | "plain" | "c" | "clear" | "u" | "unscrambled"
                 -> Plaintext (output_nl , "Enter passphrase: ")
@@ -203,63 +218,35 @@ let () =
         in begin match mode with
         | Plaintext _ | Scrambled _ ->
           parse_cmdline_queue (mode::acc) tl
-        | Systemdwatch event_list ->
+        | Systemdwatch _ ->
           [mode]
         end
-      | [] -> List.rev acc
-    in let queue =
+     | [] -> List.rev acc
+    in
+    let operation_queue =
       let argv =
         if 1 = Array.length Sys.argv then
-          [ "1" ] (* default is to ask for one passphrase *)
+          [ "1" ] (* default when no arguments is to ask for one passphrase *)
         else
           List.map (Bytes.to_string)
             (List.tl (Array.to_list Sys.argv))
       in parse_cmdline_queue [] argv
     in
-
-    let watch_mode = List.mem (Systemdwatch []) queue in
-    let open Inotify in
-    let inotify_fd = Inotify.create () in
-    let askpassword_dir =
-      if watch_mode then
-        "/run/systemd/ask-password/"
-      else
-        "/"
-    in
-    let _ = Inotify.add_watch inotify_fd askpassword_dir
-            [ S_Moved_to ; S_Close_write ] in
-
-    let rec process_queue = function
-    | hd::tl ->
-      let op_queue =
-      begin match hd with
-      | Plaintext (callback, prompt) ->
-        let () = display prompt in
-        let () = callback ((input_line (in_channel_of_descr stdin)) ^ "\n")
-        in tl
-      | Scrambled (callback, prompt) ->
-        let () = display (linux_save ^ linux_cls) in
-        let passphrase = retrieve_key_entry ~random_char ~display ~prompt in
-        let () = display (linux_cls ^ linux_restore) in
-        let () = callback passphrase
-        in tl
-      | Systemdwatch event_list ->
-        if watch_mode then
-          systemd_loopforever ~askpassword_dir ~inotify_fd ~event_list ~cb:(systemd_askpassword ~display)
-        else
-          let () = Printf.printf "SystemD watching disabled!\n"
-        in tl
-      end
-      in process_queue op_queue
-    | [] -> ()
-    in
-      if Unix.isatty Unix.stdout && not watch_mode
-      then begin
+    if operation_queue = [Systemdwatch []] then
+       handle_operation List.(hd operation_queue)
+    else begin
+        let rec process_queue op_lst : unit =
+          begin match op_lst with
+          | hd::tl ->
+              let () = handle_operation hd in process_queue tl
+          | [] -> ()
+          end
+        in
+        if Unix.isatty Unix.stdout
+        then begin
         (* display a warning if we're not using systemd and we're not redirecting the output *)
-        let () =
-        Printf.printf "You didn't redirect stdout. Error!\nUsage: (%s [mode, mode, ..]) where mode is either 'c'|'u'|'p' for unscrambled lines or an integer (count of scrambled lines)\nExample: (scramlkb c c 2 c) -> reads two plaintext lines, two scrambled lines, then one unscrambled line\n"
-        Sys.argv.(0) in 
-        Pervasives.exit 2
-      end else
-        process_queue queue
-
+          let () =
+          Printf.eprintf "You didn't redirect stdout. Error!\nUsage: (%s [mode, mode, ..]) where mode is either 'c'|'u'|'p' for unscrambled lines or an integer (count of scrambled lines)\nExample: (scramlkb c c 2 c) -> reads two plaintext lines, two scrambled lines, then one unscrambled line\n" Sys.argv.(0) in
+          Pervasives.exit 2
+        end else process_queue operation_queue
+    end
